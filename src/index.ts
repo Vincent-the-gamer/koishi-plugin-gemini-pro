2a9edf8f949d018cfb116bb024ad54d563c79174
Vincent-the-gamer <erxianqiaottjt@163.com>
2024-07-03 10:31:31 +0800

    fix: cannot build `index.js` correctly. ( #1 )

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'gemini-pro'
 
@@ -20,12 +20,12 @@ export function apply(ctx: Context) {
         // 清空上下文
         if(message === "清空上下文") {
             response = await ctx.http.get(ctx.config.url + "/clear")
-            result = `${<quote id={messageId}/>} ${response.msg}`
+            result = `${ h.quote(messageId) } ${response.msg}`
             session.send(result)
         } else {
             response = await ctx.http.post(ctx.config.url + "/chat", { message })
             // 回复消息
-            result = `${<quote id={messageId}/>} ${response.result}`
+            result = `${ h.quote(messageId) } ${response.result}`
             session.send(result)
         }
      })
